668986bcaab1b08e16ea4d9a3b0e83828a66fa08
Panelmb80 <165624707+Panelmb80@users.noreply.github.com>
2026-06-08 07:43:53 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,36 +3,31 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
+  const url = new URL(request.url);
   
-  const githubSubUrl = "https://raw.githubusercontent.com/Panelmb80/vpn1/refs/heads/main/config.txt"; 
+  if (url.pathname !== "/panel80") {
+    return new Response("Not Found", { status: 404 });
+  }
 
-  
-  const expireDate = new Date("2026-07-08T23:59:59Z"); 
-
-  
-  const totalGigabytes = 10; 
-
-  
-
+  const githubSubUrl = "https://raw.githubusercontent.com/Panelmb80/vpn1/refs/heads/main/config.txt";
+  const expireDate = new Date("2026-07-08T23:59:59Z");
+  const totalGigabytes = 10;
   const expireTimestamp = Math.floor(expireDate.getTime() / 1000);
   const totalBytes = totalGigabytes * 1024 * 1024 * 1024;
 
   try {
     let response = await fetch(githubSubUrl);
     let content = await response.text();
-
     let newResponse = new Response(content, response);
-    
+
     newResponse.headers.set(
-      'subscription-userinfo', 
+      'subscription-userinfo',
       `upload=0; download=0; total=${totalBytes}; expire=${expireTimestamp}`
     );
-    
     newResponse.headers.set('content-type', 'text/plain; charset=utf-8');
     newResponse.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
 
     return newResponse;
-
   } catch (error) {
     return new Response("Error", { status: 500 });
   }
