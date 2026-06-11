1b636db9e0eaf405f7f79d651e508be520cb547c
Panelmb80 <165624707+Panelmb80@users.noreply.github.com>
2026-06-11 14:47:59 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// گوش دادن به درخواست‌هایی که به سمت ورکر میاد
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -5,28 +6,29 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const url = new URL(request.url);
   
+  // ۱. بررسی لینک: اگر کلمه VIPMidas توی لینک نباشه، ارور ۴۰۴ میده
   if (!url.pathname.includes("VIPMidas")) {
     return new Response("Not Found", { status: 404 });
   }
 
+  // ۲. تاریخ انقضا: تنظیم شده روی تاریخ ۲۰۲۶/۰۷/۰۸
   const expireDate = new Date("2026-07-08T23:59:59Z");
   if (new Date() > expireDate) {
     return new Response("Expired", { status: 403 });
   }
 
-  let currentUsed = await MY_KV.get("used_bytes") || "0";
-  let val = parseInt(currentUsed) + (Math.floor(Math.random() * 101) + 50) * 1024 * 1024;
-  await MY_KV.put("used_bytes", val.toString());
-
+  // ۳. دریافت کانفیگ‌ها: دانلود مستقیم فایل متنی آی‌پی‌ها از گیت‌هاب شما
   const response = await fetch("https://raw.githubusercontent.com/Panelmb80/vpn1/refs/heads/main/config.txt");
   const content = await response.text();
 
+  // ۴. ارسال خروجی: تحویل کانفیگ‌ها به هیدیفای همراه با مشخصات حجم (بدون KV و حجم تصادفی)
   return new Response(content, {
     status: 200,
     headers: {
       'content-type': 'text/plain; charset=utf-8',
-      'subscription-userinfo': `upload=0; download=${val}; total=${100 * 1024 * 1024 * 1024}; expire=${Math.floor(expireDate.getTime() / 1000)}`,
-      'Cache-Control': 'no-store'
+      // در این خط مقدار download روی 0 تنظیم شده و ترافیک کل هم 100 گیگابایته
+      'subscription-userinfo': `upload=0; download=0; total=${100 * 1024 * 1024 * 1024}; expire=${Math.floor(expireDate.getTime() / 1000)}`,
+      'Cache-Control': 'no-store' // جلوگیری از ذخیره شدن آی‌پی‌های قدیمی در مرورگر یا کلاینت
     }
   });
 }
